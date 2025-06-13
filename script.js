43d0900a5fb09cb79f0a0a69f4af967a3ac75b38
BlindAxolotl <167556385+BlindAxolotl@users.noreply.github.com>
2025-06-13 18:35:04 -0500

    Added barbaric UI component instead of console component

--- script.js
+++ script.js
@@ -11,94 +11,61 @@ function getComputerChoice() {
   }
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+// Main function to examine choices and update scores
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("Draw");
+  } else if (humanChoice === "rock" && computerChoice === "paper") {
+    computerScore++;
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore++;
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
+  }
+
+  // Check if either player has reached a max score of 5, update div, clear score
+  if (humanScore > computerScore && humanScore == 5) {
+    div.textContent = `Congrats! You defeated your AI overlords with a final score of ${humanScore}!`;
+    humanScore = 0;
+    computerScore = 0;
+  } else if (humanScore < computerScore && computerScore == 5) {
+    div.textContent = `Ooops. Have fun carrying out tasks for the machine. AI defeated you with a final score of ${computerScore}.`;
+    computerScore = 0;
+    humanScore = 0;
+  } else {
+    div.textContent = `Your Score: ${humanScore} AI Overlord Score: ${computerScore}`;
+  }
+}
+
+// Store elements in variables and add event listeners
+const div = document.querySelector("#results");
 const rbutton = document.querySelector("#r");
 const pbutton = document.querySelector("#p");
 const sbutton = document.querySelector("#s");
 
 rbutton.addEventListener("click", () => {
-  console.log("Rock");
+  let choice = "rock";
+
+  playRound(choice, getComputerChoice());
 });
+
 pbutton.addEventListener("click", () => {
-  console.log("Paper");
+  let choice = "paper";
+
+  playRound(choice, getComputerChoice());
 });
+
 sbutton.addEventListener("click", () => {
-  console.log("Scissors");
+  let choice = "scissors";
+
+  playRound(choice, getComputerChoice());
 });
-
-function getHumanChoice() {
-  // Prompt user for choice #
-  let choice = prompt("Enter a number: \n1: Rock \n2: Paper \n3: Scissors");
-
-  // If user choice matches available options, return human choice as a string
-  if (Number(choice) === 1) {
-    return "rock";
-  } else if (Number(choice) === 2) {
-    return "paper";
-  } else if (Number(choice) === 3) {
-    return "scissors";
-  } else {
-    return "Not a valid selection....";
-  }
-}
-
-function playGame() {
-  // Declaring score variables
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    console.log(
-      "You chose: " + humanChoice + "\nComputer chose: " + computerChoice,
-    );
-
-    // Writing results for each case and updating score accordingly
-    if (humanChoice === computerChoice) {
-      console.log("You picked the same! It's a Tie!");
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-      console.log("You lost...Paper beats Rock");
-      computerScore++;
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-      console.log("You win! Rock beats Scissors");
-      humanScore++;
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-      console.log("You win! Paper beats Rock");
-      humanScore++;
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-      console.log("You lost...Scissors beats Paper");
-      computerScore++;
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-      console.log("You lost...Rock beats Scissors");
-      computerScore++;
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-      console.log("You win! Scissors beats Paper");
-      humanScore++;
-    }
-  }
-
-  // Determining winner of the game
-  if (humanScore > computerScore) {
-    console.log(
-      "You win! Your score was: " +
-        humanScore +
-        "\nComputer score was: " +
-        computerScore,
-    );
-  } else if (humanScore < computerScore) {
-    console.log(
-      "You lost.... Your score was: " +
-        humanScore +
-        "\nComputer score was: " +
-        computerScore,
-    );
-  } else {
-    console.log(
-      "It was a tie :/ Your score was: " +
-        humanScore +
-        "\nComputer score was: " +
-        computerScore,
-    );
-  }
-}
-
-playGame();
-
